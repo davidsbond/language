d91c9e8a0c11ee428bb46648e6fc97693981fbdc
davidsbond <davidsbond93@gmail.com>
2019-03-08 23:05:17 +0000

    Added evaluator test for new form of function call

---
+++ README.md
@@ -0,0 +1,16 @@
+# Language
+
+```dave
+func main() {
+    const first = 1
+    var second = 2
+
+    const third = add(first, second)
+
+    return third
+}
+
+func add(a, b) {
+    return a + b
+}
+```
--- evaluator/call_expression_test.go
+++ evaluator/call_expression_test.go
@@ -21,6 +21,19 @@ func TestEvaluator_CallExpression(t *testing.T) {
 		ExpectedObject object.Object
 	}{
 		{
+			Name: "It should evaluate valid, inline call expressions",
+			Expression: `
+			var add = func(a, b) {
+				return a + b
+			}
+
+			add(1, 2)
+			`,
+			ExpectedObject: &object.Number{
+				Value: 3,
+			},
+		},
+		{
 			Name: "It should evaluate valid call expressions",
 			Expression: `
 			func add(a, b) {
