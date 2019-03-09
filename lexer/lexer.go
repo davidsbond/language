ac82ae1fe07e1cc4f32f802b45de60ffdcc9d270
davidsbond <davidsbond93@gmail.com>
2019-03-09 22:52:17 +0000

    Made lexer return tokens for brackets

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -68,6 +68,10 @@ func (l *Lexer) NextToken() (tok *token.Token, err error) {
 		tok = token.New(token.LBRACE, token.LBRACE, l.line, l.column)
 	case '}':
 		tok = token.New(token.RBRACE, token.RBRACE, l.line, l.column)
+	case '[':
+		tok = token.New(token.LBRACKET, token.LBRACKET, l.line, l.column)
+	case ']':
+		tok = token.New(token.RBRACKET, token.RBRACKET, l.line, l.column)
 	case '=':
 		var next rune
 
