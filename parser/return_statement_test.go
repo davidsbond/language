661df26d496ef8fe5b1ff5780c8695257a7d40ae
davidsbond <davidsbond93@gmail.com>
2019-03-09 01:59:10 +0000

    Added missing return value tests

--- parser/return_statement_test.go
+++ parser/return_statement_test.go
@@ -32,6 +32,28 @@ func TestParser_ReturnStatement(t *testing.T) {
 				},
 			},
 		},
+		{
+			Name:       "It should parse numbers",
+			Expression: `return 1`,
+			ExpectedLiteral: &ast.ReturnStatement{
+				Token: token.New("return", token.RETURN, 0, 0),
+				ReturnValue: &ast.NumberLiteral{
+					Token: token.New("1", token.NUMBER, 0, 0),
+					Value: 1,
+				},
+			},
+		},
+		{
+			Name:       "It should parse characters",
+			Expression: `return '1'`,
+			ExpectedLiteral: &ast.ReturnStatement{
+				Token: token.New("return", token.RETURN, 0, 0),
+				ReturnValue: &ast.CharacterLiteral{
+					Token: token.New("'", token.CHAR, 0, 0),
+					Value: '1',
+				},
+			},
+		},
 	}
 
 	for _, tc := range tt {
