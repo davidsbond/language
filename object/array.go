aa4357c147c86d033a59ec231ba25f7e69bac83e
davidsbond <davidsbond93@gmail.com>
2019-03-10 17:41:16 +0000

    Added missing array docs

--- object/array.go
+++ object/array.go
@@ -3,19 +3,23 @@ package object
 import "strings"
 
 const (
+	// TypeArray is the type for an array value
 	TypeArray = "Array"
 )
 
 type (
+	// The Array type represents an array of objects
 	Array struct {
 		Elements []Object
 	}
 )
 
+// Type returns this object's type.
 func (ar *Array) Type() Type {
 	return TypeArray
 }
 
+// Clone returns a copy of the array as a new pointer.
 func (ar *Array) Clone() Object {
 	return &Array{Elements: ar.Elements}
 }
