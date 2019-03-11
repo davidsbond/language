e4b77cdb46b1de1b326646167738455c9f0a93b1
davidsbond <davidsbond93@gmail.com>
2019-03-11 21:41:18 +0000

    Added repl loop to main function

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
 
+	"github.com/davidsbond/dave/evaluator"
+
+	"github.com/davidsbond/dave/lexer"
+	"github.com/davidsbond/dave/object"
+	"github.com/davidsbond/dave/parser"
+)
+
+func main() {
+	scope := object.NewScope()
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("> ")
+
+repl:
+	for scanner.Scan() {
+		line := bytes.NewBuffer(scanner.Bytes())
+
+		lexer, _ := lexer.New(bufio.NewReader(line))
+		parser := parser.New(lexer)
+		ast, errs := parser.Parse()
+
+		for _, err := range errs {
+			fmt.Println(err.Error())
+			fmt.Print("> ")
+			line.Reset()
+
+			continue repl
+		}
+
+		if result := evaluator.Evaluate(ast, scope); result != nil {
+			fmt.Println(result.String())
+		}
+
+		fmt.Print("> ")
+	}
 }
